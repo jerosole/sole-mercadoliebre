803eb212543da7834272f7ad5999ca73e82df6b2
jerosole <jerojsole@gmail.com>
2023-08-19 14:03:34 -0300

    trabajo casi completo

--- src/app.js
+++ src/app.js
@@ -6,10 +6,18 @@ const app = express();
 app.use(express.static(path.join(__dirname, "../public")))
 
 const PORT = 3001
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log('se prendio en el puerto 3001')
 });
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views/home.html"))
 })
+app.get('/register', (req, res) => {
+    let htmlPath = path.join(__dirname, "./views/register.html");
+    res.sendFile(htmlPath);
+});
+app.get('/login', (req, res) => {
+    let htmlPath = path.join(__dirname, "./views/login.html");
+    res.sendFile(htmlPath);
+});
